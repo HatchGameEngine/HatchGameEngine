5d873ec124806e95258ebad11a04bbbd553029b6
Axanery <41282531+Axanery@users.noreply.github.com>
2025-01-06 15:37:54 -0600

    Change location of Dev Menu fonts

--- source/Engine/Scene.cpp
+++ source/Engine/Scene.cpp
@@ -1363,8 +1363,8 @@ void Scene::Restart() {
         Scene::SetTileCount(Scene::BaseTileCount);
     }
 
-    Application::DeveloperDarkFont = Application::LoadDevFont("Sprites/UI/DarkFont.bin");
-    Application::DeveloperLightFont = Application::LoadDevFont("Sprites/UI/SmallFont.bin");
+    Application::DeveloperDarkFont = Application::LoadDevFont("Sprites/Fonts/DarkFont.bin");
+    Application::DeveloperLightFont = Application::LoadDevFont("Sprites/Fonts/SmallFont.bin");
 
     // Restart tile animations
     for (Tileset& tileset : Scene::Tilesets)
